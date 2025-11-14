50766951b3e285e65474418f451809220e43a015
7sumona02 <sumona.23548@sscbs.du.ac.in>
2025-11-14 16:08:03 +0530

    added

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,69 @@
-import Image from "next/image";
+import { Card } from '@/components/ui/card'
+import { InputGroup, InputGroupAddon, InputGroupInput } from '@/components/ui/input-group'
+import { Search } from 'lucide-react'
 
-export default function Home() {
+const page = async () => {
+  const res = await fetch("https://dummyjson.com/products");
+  const data = await res.json();
+  const products = data.products
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+    <div className='max-w-5xl mx-auto flex flex-col justify-center items-center pt-26 pb-10'>
+      <Nav />
+      <div className='grid grid-cols-3 space-x-5 space-y-12'>
+        {products.map((product: any) => (
+          <ProductCard
+            key={product.id}
+            imgUrl={product.thumbnail}
+            title={product.title}
+            price={`$${product.price}`}
+            description={product.description}
+          />
+        ))}
+      </div>
     </div>
-  );
+  )
+}
+
+export default page
+
+const ProductCard = ({ imgUrl, title, price, description }: { imgUrl: string, title: string, price: string, description: string }) => {
+   const shortDescription =
+    description.length > 40
+      ? description.slice(0, 40) + "..."
+      : description;
+
+      const shortTitle =
+    title.length > 22
+      ? title.slice(0, 22) + "..."
+      : title;
+  return (
+    <Card className='w-[20vw] p-1 pb-3'>
+      <div className='w-full h-60 overflow-hidden rounded-md'>
+        <img src={imgUrl} className='w-full h-full object-cover' alt='product-image' />
+      </div>
+      <div className='px-1.5'>
+        <div className='w-full flex items-center justify-between mt-2'>
+          <div className='font-medium'>{shortTitle}</div>
+          <div className='font-medium text-sm'>{price}</div>
+        </div>
+        <div className='text-sm text-neutral-500 mt-1'>{shortDescription}</div>
+      </div>
+    </Card>
+  )
+}
+
+const Nav = () => {
+  return (
+    <div className='w-screen flex justify-between items-center fixed top-0 py-3 px-10 bg-white'>
+      <div className='text-xl font-medium tracking-tight'>Product <span className='text-neutral-400'>Explorer.</span></div>
+      <div>
+        <InputGroup className='w-xs'>
+          <InputGroupInput placeholder='Search product...' />
+          <InputGroupAddon>
+            <Search />
+          </InputGroupAddon>
+        </InputGroup>
+      </div>
+    </div>
+  )
 }
